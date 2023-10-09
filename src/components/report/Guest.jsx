9a84c2d3ed8bd424aaa61d0cc27c22640f04b515
just4fun147 <pauluswindito1@gmail.com>
2023-10-09 12:39:18 +0700

    New Guest Report

--- src/components/report/Guest.jsx
+++ src/components/report/Guest.jsx
@@ -1,106 +1,149 @@
 import logo from "../../assets/fhdtrans.png";
 import Table from "react-bootstrap/Table";
-import React from "react";
-// import "./Table.css";
-const NewCust = () => {
+import axios from "axios";
+import { headersAuth } from "../../Api";
+import React, { useEffect, useState } from "react";
+import Loading from "../Loading";
+
+const NewCust = (props) => {
+  const [loadings, setLoadings] = useState(false);
+  const [total, setTotal] = useState();
+  const [today, setToday] = useState();
+  const [data, setData] = useState();
+  const getData = () => {
+    setLoadings(true);
+    return new Promise((resolve) => {
+      axios
+        .post(
+          process.env.REACT_APP_BASEURL + "report/guest",
+          { year: props.search, month: props.month },
+          {
+            headers: headersAuth,
+          }
+        )
+        .then((response) => {
+          setData(response.data.OUT_DATA.data);
+          setTotal(response.data.OUT_DATA.total);
+          setToday(response.data.OUT_DATA.today);
+          setLoadings(false);
+        })
+        .catch((error) => {
+          setLoadings(false);
+        });
+    });
+  };
+  useEffect(() => {
+    getData();
+  }, []);
   return (
     <>
-      <div className="container" style={{ alignItems: "center" }}>
-        <img
-          src={logo}
-          alt="logo"
-          style={{ width: "100%", maxWidth: "500px" }}
-        ></img>
-        <p style={{ fontSize: "1.25rem" }}>
-          Jl. P. Mangkubumi No.18, Yogyakarta 55233
-        </p>
-        <p style={{ fontSize: "1.25rem", lineHeight: "0.5" }}>
-          Telp. (0274) 487711
-        </p>
-        <hr />
-      </div>
-      <div className="row">
-        <h3 style={{ fontWeight: "bold", fontSize: "3rem" }}>
-          LAPORAN JUMLAH TAMU
-        </h3>
-      </div>
-      <div className="container row" style={{ textAlign: "start" }}>
-        <p style={{ fontSize: "1.2rem" }}>Tahun : xxxx</p>
-      </div>
-      <div className="container row" style={{ textAlign: "start" }}>
-        <p style={{ fontSize: "1.2rem" }}>Bulan : Januari</p>
-      </div>
-      <div className="container mb-5">
-        <Table
-          bordered
-          striped
-          style={{ borderColor: "black", border: "3px solid" }}
-        >
-          <thead style={{ borderColor: "black", border: "3px solid" }}>
-            <tr style={{ borderColor: "black", border: "1px solid" }}>
-              <th style={{ borderColor: "black", border: "1px solid" }}>No</th>
-              <th style={{ borderColor: "black", border: "1px solid" }}>
-                Jenis Kamar
-              </th>
-              <th style={{ borderColor: "black", border: "1px solid" }}>
-                Grup
-              </th>
-              <th style={{ borderColor: "black", border: "1px solid" }}>
-                Personal
-              </th>
-              <th style={{ borderColor: "black", border: "1px solid" }}>
-                Total
-              </th>
-            </tr>
-          </thead>
-          <tbody style={{ borderColor: "black", border: "3px solid" }}>
-            <tr style={{ borderColor: "black", border: "1px solid" }}>
-              <td style={{ borderColor: "black", border: "1px solid" }}> 1 </td>
-              <td style={{ borderColor: "black", border: "1px solid" }}>
-                Superior
-              </td>
-              <td style={{ borderColor: "black", border: "1px solid" }}>20</td>
-              <td style={{ borderColor: "black", border: "1px solid" }}>10</td>
-              <td style={{ borderColor: "black", border: "1px solid" }}>30</td>
-            </tr>
-            <tr>
-              <td> 1 </td>
-              <td> Superior </td>
-              <td> 20 </td>
-              <td> 10 </td>
-              <td> 30 </td>
-            </tr>
-            <tr>
-              <td> 1 </td>
-              <td> Junior Suite </td>
-              <td> 20 </td>
-              <td> 10 </td>
-              <td> 30 </td>
-            </tr>
-            <tr style={{ borderColor: "black", border: "1px solid" }}>
-              <td
-                colSpan={4}
-                style={{ borderColor: "black", border: "1px solid" }}
-              >
-                Total
-              </td>
-              <td style={{ borderColor: "black", border: "1px solid" }}>300</td>
-            </tr>
-            {/* {users.map((user) => (
-                      <tr key={user.id}>
-                        <td> {user.full_name} </td>
-                        <td> {user.name}</td>
-                        <td> {user.email}</td>
-                        <td> {user.domisili}</td>
-                        <td> {user.pekerjaan}</td>
-                        <td> {user.no_telp}</td>
-                        <td> {user.sosmed}</td>
-                      </tr>
-                    ))} */}
-          </tbody>
-        </Table>
-      </div>
-      <p style={{ textAlign: "end" }}>Dicetak tanggal xx Januari 2023</p>
+      {!loadings ? (
+        <>
+          <div className="container" style={{ alignItems: "center" }}>
+            <img
+              src={logo}
+              alt="logo"
+              style={{ width: "100%", maxWidth: "500px" }}
+            ></img>
+            <p style={{ fontSize: "1.25rem" }}>
+              Jl. P. Mangkubumi No.18, Yogyakarta 55233
+            </p>
+            <p style={{ fontSize: "1.25rem", lineHeight: "0.5" }}>
+              Telp. (0274) 487711
+            </p>
+            <hr />
+          </div>
+          <div className="row">
+            <h3 style={{ fontWeight: "bold", fontSize: "3rem" }}>
+              LAPORAN JUMLAH TAMU
+            </h3>
+          </div>
+          <div className="container row" style={{ textAlign: "start" }}>
+            <p style={{ fontSize: "1.2rem" }}>Tahun : {props.search}</p>
+          </div>
+          <div className="container row" style={{ textAlign: "start" }}>
+            <p style={{ fontSize: "1.2rem" }}>Bulan : {props.month}</p>
+          </div>
+          <div className="container mb-5">
+            <Table
+              bordered
+              striped
+              style={{ borderColor: "black", border: "3px solid" }}
+            >
+              <thead style={{ borderColor: "black", border: "3px solid" }}>
+                <tr style={{ borderColor: "black", border: "1px solid" }}>
+                  <th style={{ borderColor: "black", border: "1px solid" }}>
+                    No
+                  </th>
+                  <th style={{ borderColor: "black", border: "1px solid" }}>
+                    Jenis Kamar
+                  </th>
+                  <th style={{ borderColor: "black", border: "1px solid" }}>
+                    Grup
+                  </th>
+                  <th style={{ borderColor: "black", border: "1px solid" }}>
+                    Personal
+                  </th>
+                  <th style={{ borderColor: "black", border: "1px solid" }}>
+                    Total
+                  </th>
+                </tr>
+              </thead>
+              <tbody style={{ borderColor: "black", border: "3px solid" }}>
+                {data.map((d) => (
+                  <tr
+                    key={d.id}
+                    style={{ borderColor: "black", border: "1px solid" }}
+                  >
+                    <td style={{ borderColor: "black", border: "1px solid" }}>
+                      {" "}
+                      {d.no}{" "}
+                    </td>
+                    <td
+                      style={{
+                        borderColor: "black",
+                        border: "1px solid",
+                        textAlign: "start",
+                      }}
+                    >
+                      {" "}
+                      {d.type_name}
+                    </td>
+                    <td style={{ borderColor: "black", border: "1px solid" }}>
+                      {" "}
+                      {d.group}
+                    </td>
+                    <td style={{ borderColor: "black", border: "1px solid" }}>
+                      {" "}
+                      {d.personal}
+                    </td>
+                    <td style={{ borderColor: "black", border: "1px solid" }}>
+                      {" "}
+                      {d.total}
+                    </td>
+                  </tr>
+                ))}
+                <tr style={{ borderColor: "black", border: "1px solid" }}>
+                  <td
+                    colSpan={4}
+                    style={{ borderColor: "black", border: "1px solid" }}
+                  >
+                    Total
+                  </td>
+                  <td style={{ borderColor: "black", border: "1px solid" }}>
+                    {total}
+                  </td>
+                </tr>
+              </tbody>
+            </Table>
+          </div>
+          <p style={{ textAlign: "end" }}>Dicetak tanggal {today}</p>
+        </>
+      ) : (
+        <>
+          <Loading />
+        </>
+      )}
     </>
   );
 };
--- src/pages/Report/GuestReport.jsx
+++ src/pages/Report/GuestReport.jsx
@@ -2,7 +2,44 @@ import JsPDF from "jspdf";
 import Button from "react-bootstrap/Button";
 import Guest from "../../components/report/Guest";
 import html2canvas from "html2canvas";
+import axios from "axios";
+import { headersAuth } from "../../Api";
+import React, { useEffect, useState } from "react";
+import Loading from "../../components/Loading";
 const GuestReport = () => {
+  const [search, setSearch] = useState("0");
+  const [loading, setLoading] = useState(true);
+  const [year, setYear] = useState();
+  const [month, setMonth] = useState();
+  const [select, setSelect] = useState(false);
+  const checkSelect = () => {
+    if (month != null && year != null) {
+      setSelect(true);
+    }
+    if (month != "" && year != "") {
+      setSelect(true);
+    }
+  };
+  const getYear = () => {
+    return new Promise((resolve) => {
+      axios
+        .post(
+          process.env.REACT_APP_BASEURL + "report/getYear",
+          {},
+          {
+            headers: headersAuth,
+          }
+        )
+        .then((response) => {
+          setYear(response.data.OUT_DATA);
+          setLoading(false);
+        })
+        .catch((error) => {});
+    });
+  };
+  useEffect(() => {
+    getYear();
+  }, []);
   const generatePDF = () => {
     const input = document.getElementById("report");
     html2canvas(input).then((canvas) => {
@@ -24,26 +61,105 @@ const GuestReport = () => {
   return (
     <>
       <div className="container">
-        <div className="row mt-5" style={{ justifyContent: "end" }}>
-          <Button style={{ width: "fit-content" }} onClick={generatePDF}>
-            {" "}
-            download
-          </Button>
-        </div>
-      </div>
-      <div
-        className="container mt-5 mb-3"
-        style={{
-          width: "100%",
-          paddingRight: "5rem",
-          paddingLeft: "5rem",
-          paddingTop: "3rem",
-          paddingBottom: "3rem",
-          backgroundColor: "white",
-        }}
-        id="report"
-      >
-        <Guest />
+        {loading ? (
+          <>
+            <Loading />
+          </>
+        ) : (
+          <>
+            <div className="row mt-5" style={{ textAlign: "start" }}>
+              <div className="col-5">
+                <p>Select Year</p>
+              </div>
+              <div className="col-5">
+                <p>Select Month</p>
+              </div>
+            </div>
+            <div className="row" style={{ textAlign: "start" }}>
+              <div className="col-5">
+                <select
+                  style={{
+                    width: "100%",
+                    minWidth: "250px",
+                    display: "block",
+                    marginRight: "auto",
+                    marginLeft: "auto",
+                    backgroundColor: "#D9D9D9",
+                    borderRadius: "5px",
+                    lineHeight: "0.25",
+                  }}
+                  onChange={(e) => {
+                    setSearch(e.target.value);
+                  }}
+                >
+                  {year.map((y) => (
+                    <option value={y.year}>{y.year}</option>
+                  ))}
+                </select>
+              </div>
+              <div className="col-5">
+                <select
+                  style={{
+                    width: "100%",
+                    minWidth: "250px",
+                    display: "block",
+                    marginRight: "auto",
+                    marginLeft: "auto",
+                    backgroundColor: "#D9D9D9",
+                    borderRadius: "5px",
+                    lineHeight: "0.25",
+                  }}
+                  onChange={(e) => {
+                    setMonth(e.target.value);
+                  }}
+                >
+                  <option value="1">Januari</option>
+                  <option value="2">Februari</option>
+                  <option value="3">Maret</option>
+                  <option value="4">April</option>
+                  <option value="5">Mei</option>
+                  <option value="6">Juni</option>
+                  <option value="7">Juli</option>
+                  <option value="8">Agustus</option>
+                  <option value="9">September</option>
+                  <option value="10">Oktober</option>
+                  <option value="11">November</option>
+                  <option value="12">Desember</option>
+                </select>
+              </div>
+              {month == null || search == null ? (
+                <></>
+              ) : (
+                <>
+                  <div className="col-2">
+                    <Button
+                      style={{ width: "fit-content" }}
+                      onClick={generatePDF}
+                    >
+                      {" "}
+                      download
+                    </Button>
+                  </div>
+                  <div
+                    className="container mt-5 mb-3"
+                    style={{
+                      width: "100%",
+                      paddingRight: "5rem",
+                      paddingLeft: "5rem",
+                      paddingTop: "3rem",
+                      paddingBottom: "3rem",
+                      backgroundColor: "white",
+                      textAlign: "center",
+                    }}
+                    id="report"
+                  >
+                    <Guest search={search} month={month} />
+                  </div>
+                </>
+              )}
+            </div>
+          </>
+        )}
       </div>
     </>
   );
